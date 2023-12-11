7b2aceaf791d63febf9716895ca4d5afa6a4ae68
Zack Yule <yuezhao@nwu.edu.cn>
2023-12-11 09:57:15 +0800

    update

--- app/routes/chat_routes.py
+++ app/routes/chat_routes.py
@@ -1,8 +1,7 @@
 from typing import Optional
 from fastapi import APIRouter, HTTPException, Header, Depends
-
 from app.utils.key_pool_request_sender import processor_instance
-from ..models.chat_models import ChatMessage, ChatRequest, ChatResponse, Choice, Usage
+from ..models.chat_models import ChatRequest
 import logging
 from app.logger_config import setup_logging
 
@@ -10,16 +9,31 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
+# 身份验证依赖
+async def verify_token(authorization: Optional[str] = Header(None)):
+    if not authorization or not authorization.startswith("Bearer "):
+        logger.warning("Unauthorized access attempt")
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return authorization
+
+
+async def process_chat_completion(request: ChatRequest):
+    try:
+        request_body = request.model_dump()
+        response = await processor_instance.send_request(request_body)
+        return response
+    except Exception as e:
+        logger.error(f"Error processing request: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
 @router.post("/chat/completions")
 async def chat_completions(request: ChatRequest,
-                           authorization: Optional[str] = Header(None)):
-    if not authorization or not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Unauthorized")
+                           authorization: str = Depends(verify_token)):
+    return await process_chat_completion(request)
 
-    # 读取请求主体
-    request_body = request.model_dump()
 
-    # 调用异步的 send_request 函数
-    response = await processor_instance.send_request(request_body)
-
-    return response
+@router.post("/v1/chat/completions")
+async def chat_completions_v1(request: ChatRequest,
+                              authorization: str = Depends(verify_token)):
+    return await process_chat_completion(request)
