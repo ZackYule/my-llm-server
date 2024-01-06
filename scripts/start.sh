3241277414f7600160db5e47594aefb85a1166b4
Zack Yule <yuezhao@nwu.edu.cn>
2024-01-06 18:43:53 +0800

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,6 @@ cd `dirname $0`/..
 export BASE_DIR=`pwd`
 echo $BASE_DIR
 
-nohup pipenv run uvicorn app.main:app --reload --timeout-keep-alive 60 --port 8080 & tail -f "${BASE_DIR}/nohup.out"
+nohup pipenv run uvicorn app.main:app --reload --timeout-keep-alive 60 --port 8080 &
 
 echo "my-llm-server is starting，you can check the ${BASE_DIR}/nohup.out"
